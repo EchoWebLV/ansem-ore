6b79388770962fad225693b5cc97c5fb4580b514
darkzer0isback <yordan.lasonov@gmail.com>
2026-07-06 12:48:48 +0300

    chore: scaffold ansem-miner anchor workspace

---
+++ programs/ansem-miner/src/lib.rs
@@ -0,0 +1,15 @@
+use anchor_lang::prelude::*;
+
+declare_id!("8Q9EnK7ydn6ywo7ZxeqhubqYybf7FFNNwnz8JzJjXZjz");
+
+#[program]
+pub mod ansem_miner {
+    use super::*;
+
+    pub fn ping(_ctx: Context<Ping>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Ping {}
